6551826e6cafae0918589b84939d7f756a68854c
yassermuayqilalsaedi <vyasseraa1101@gmail.com>
2026-05-19 04:37:34 +0300

    Add shared protocol constants

---
+++ src/main/java/Protocol.java
@@ -0,0 +1,20 @@
+public class Protocol {
+
+    public static final String ADD_CUSTOMER     = "ADD_CUSTOMER";
+    public static final String ADD_CAR          = "ADD_CAR";
+    public static final String BOOK_APPOINTMENT = "BOOK_APPOINTMENT";
+    public static final String UPDATE_STATUS     = "UPDATE_STATUS";
+    public static final String VIEW_APPOINTMENTS = "VIEW_APPOINTMENTS";
+    public static final String VIEW_CUSTOMERS    = "VIEW_CUSTOMERS";
+    public static final String EXIT              = "EXIT";
+
+    public static final String SUCCESS  = "SUCCESS";
+    public static final String ERROR    = "ERROR";
+    public static final String DATA     = "DATA";
+
+    public static final String DELIMITER = "|";
+    public static final String REGEX_DELIMITER = "\\|";
+    
+    public static final int PORT = 8000;
+    public static final String HOST = "localhost";
+}
